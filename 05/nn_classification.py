ad40b9809900fadb9faa3f1fb0bee6ae522dc298
端木胥 <1571170555@qq.com>
2019-04-10 23:22:15 +0800

    update vsc.xml

--- 05/nn_classification.py
+++ 05/nn_classification.py
@@ -3,4 +3,20 @@ import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 from sklearn import neighbors, datasets
 
+#定义一个函数，原来源码中2.X版本得utilities包不存在了，读取文件需要换个方式
+def load_data(input_file):
+    a = []
+    with open(input_file, 'r') as f:
+        for line in f.readlines():
+            data = [float(x) for x in line.split(',')]
+            a.append(data)
+
+    return np.array(a)
+
 #加载数据
+input_file = 'data_nn_classifier.txt'
+data = load_data(input_file)
+X, y = data[:, :-1],data[:, -1].astype(np.int)
+
+#print(X, y)
+
