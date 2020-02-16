e5ed055207aa2836c2bbd318aa7db4b5b8fe2783
aptx4869hrj <157117055@qq.com>
2020-02-16 22:27:45 +0800

    update vsc.xml

--- 02/logistic_regression.py
+++ 02/logistic_regression.py
@@ -31,17 +31,17 @@ def plot_classifier(classifier, X, y):
 
     plt.show()
 
+if __name__=='__main__':
+    # 创建样本数据，假设有三个类
+    X = np.array([[4, 7], [3.5, 8], [3.1, 6.2], [0.5, 1], [1, 2], [1.2, 1.9], [6, 2], [5.7, 1.5], [5.4, 2.2]])
+    y = np.array([0, 0, 0, 1, 1, 1, 2, 2, 2])
 
-# 创建样本数据，假设有三个类
-X = np.array([[4, 7], [3.5, 8], [3.1, 6.2], [0.5, 1], [1, 2], [1.2, 1.9], [6, 2], [5.7, 1.5], [5.4, 2.2]])
-y = np.array([0, 0, 0, 1, 1, 1, 2, 2, 2])
+    # 初始化一个逻辑回归分类器
+    # 参数solver用于设置求解系统方程的算法类型，参数C表示正则化强度，数值越小，表示正则化强度越高
+    classifier = linear_model.LogisticRegression(solver='liblinear', C=100)
 
-# 初始化一个逻辑回归分类器
-# 参数solver用于设置求解系统方程的算法类型，参数C表示正则化强度，数值越小，表示正则化强度越高
-classifier = linear_model.LogisticRegression(solver='liblinear', C=100)
+    # 训练分类器
+    classifier.fit(X, y)
 
-# 训练分类器
-classifier.fit(X, y)
-
-# 画出数据点和边界
-plot_classifier(classifier, X, y)
+    # 画出数据点和边界
+    plot_classifier(classifier, X, y)
